79b52f567642a7d216fd0b2bae02e91bb75319e5
Ramazan HABER <ramazanhaberr@gmail.com>
2023-06-23 12:36:49 +0300

    ff

--- Pos/Main.cs
+++ Pos/Main.cs
@@ -416,7 +416,7 @@ namespace Pos
                 }
 
 
-                this.Text = "RMOS Ultimate POS [" + dbtools.database + "] v0.3.41";
+                this.Text = "RMOS Ultimate POS [" + dbtools.database + "] v0.3.42";
 
 
 
