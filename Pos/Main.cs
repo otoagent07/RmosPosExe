49e8fe5b7b942c9b2376527881bc908fa30d6641
RAMBO <RAMBO@RAMBO3>
2024-02-23 18:36:15 +0300

    53

--- Pos/Main.cs
+++ Pos/Main.cs
@@ -418,7 +418,7 @@ namespace Pos
                 }
 
 
-                this.Text = "RMOS Ultimate POS [" + dbtools.database + "] v0.3.88";
+                this.Text = "RMOS Ultimate POS [" + dbtools.database + "] v0.3.89";
 
 
 
