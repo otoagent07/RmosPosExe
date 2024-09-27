2403a5d1ab4b806864b1ccb4c7458b4412980870
ramazanhaber <ramazanhaberr@gmail.com>
2024-09-27 17:57:19 +0300

    sdas

--- Pos/Main.cs
+++ Pos/Main.cs
@@ -420,7 +420,7 @@ namespace Pos
                 }
 
 
-                this.Text = "RMOS Ultimate POS [" + dbtools.database + "] v0.4.50";
+                this.Text = "RMOS Ultimate POS [" + dbtools.database + "] v0.4.51";
 
 
 
