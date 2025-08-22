091a5bbd88288898895e7987a6836726d0e2b7fa
Ramazan <Ramazan@RAMBO3>
2025-08-22 11:58:45 +0300

    aa

--- Pos/Main.cs
+++ Pos/Main.cs
@@ -421,7 +421,7 @@ namespace Pos
                 }
 
 
-                this.Text = "RMOS Ultimate POS [" + dbtools.database + "] v0.5.70"; // aaaa
+                this.Text = "RMOS Ultimate POS [" + dbtools.database + "] v0.5.71"; // aaaa
 
 
 
