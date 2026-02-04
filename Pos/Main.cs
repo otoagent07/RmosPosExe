538104d845a228a727055cad2f498eb857710262
Ramazan <Ramazan@RAMBO3>
2026-02-04 18:39:14 +0300

    aa

--- Pos/Main.cs
+++ Pos/Main.cs
@@ -296,7 +296,7 @@ namespace Pos
         public static string MyClass = "Main";
         CallerId c = null;
 
-        public static string versiyonno = "v0.6.6";
+        public static string versiyonno = "v0.6.7";
         public void departmanYukleNew()
         {
             try
