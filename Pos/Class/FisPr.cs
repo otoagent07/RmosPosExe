f247872a5771e64274dab13678e4f15e087a03be
Ramazan <Ramazan@RAMBO3>
2026-05-01 15:03:03 +0300

    asdd

--- Pos/Class/FisPr.cs
+++ Pos/Class/FisPr.cs
@@ -7521,7 +7521,7 @@ left join Rmosmuh.dbo.Pos_User as Garson on Garson.P_Kod = Rsat_Garson
 left join Rmosmuh.dbo.Pos_User as Garson2 on Garson2.P_Kod = Rsat_Garson2
 left join pos_kodlar as poskod on poskod.Pkod_Sinif='16' and poskod.Pkod_Posta=rez.konumposta and poskod.Pkod_Ustgrup=rez.ustgrup and poskod.Pkod_Altgrup=rez.altgrup
 
-where  Rsat_Fisno='" + Fisno + @"' and rec.Rec_SiparisCikmasin=0 and Rsat_SiparisPr=0 and isnull(poskod.Pkod_AbuyerPr,'')<>''";
+where  Rsat_Fisno='" + Fisno + @"' and rec.Rec_SiparisCikmasin=0 and Rsat_SiparisPr=0 and isnull(poskod.Pkod_AbuyerPr,'')<>'' order by Rsat_Id ";
 
                         DataTable dtAbuyer = dbtools.SelectTableR(q2);
 
--- Pos/Main.cs
+++ Pos/Main.cs
@@ -296,7 +296,7 @@ namespace Pos
         public static string MyClass = "Main";
         CallerId c = null;
 
-        public static string versiyonno = "v0.7.3";
+        public static string versiyonno = "v0.7.4";
         public void departmanYukleNew()
         {
             try
